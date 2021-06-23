73153c1d82c2cfce42e3887ea038be3af0c77fff
Mariusz Błaszczyk <mariusz.blaszczyk@tuta.io>
2021-06-23 12:31:23 +0200

    first

--- src/zad1/app/App.java
+++ src/zad1/app/App.java
@@ -1,12 +1,59 @@
 package zad1.app;
 
-import zad1.io.DataGenerate;
+import java.util.Random;
 
 public class App {
 
+    static int generateNumber(int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("Random range is not correct");
+        }
+        return new Random().nextInt(max - min + 1) + min;
+    }
+
+    private static final String[] numNames = {
+            "",
+            " zero",
+            " jeden",
+            " dwa",
+            " trzy",
+            " cztery",
+            " pięć",
+            " sześć",
+            " siedem",
+            " osiem",
+            " dziewięć",
+            " dziesięć",
+            " jedenaście",
+            " dwanaście",
+            " trzynaście",
+            " czternaście",
+            " piętnaście",
+            " szesnaście",
+            " siedemnaście",
+            " osiemnaście",
+            " dziewiętnaście"
+    };
+
+    private static final String[] tensNames = {
+            "",
+            " dziesięć",
+            " dwadzieścia",
+            " trzydzieści",
+            " czterdzieści",
+            " pięćdziesiąt",
+            " sześćdziesiąt",
+            " siedemdziesiąt",
+            " osiemdziesiąt",
+            " dziewięćdziesiąt"
+    };
+
+    private final static int MIN = 0;
+    private final static int MAX = 999;
+
     public static void main(String[] args) {
 
-        int number = DataGenerate.generateNumber();
+        int number = generateNumber(MIN, MAX);
         System.out.println("The number drawn is: " + number);
 
 
